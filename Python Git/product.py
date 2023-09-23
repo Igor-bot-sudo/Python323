414f7961674dd8435b6d603e840cdca9c93933e7
Igor-bot-sudo <84375215+Igor-bot-sudo@users.noreply.github.com>
2023-09-23 22:30:39 +0300

    product.py added

--- Python Git/product.py
+++ Python Git/product.py
@@ -1,11 +1,19 @@
 class Product:
 
-    def __init__(self, name, cost, type):
+    def __init__(self, name: str, cost: float, type_: str):
         self.name = name
         self.cost = cost
-        self.type = type
+        self.type_ = type_
 
     def update_cost(self, cost):
         if cost < 0:
             return
         self.cost = cost
+    
+    def product_price(self, weight):
+        return self.cost * weight
+
+
+cucumbers = Product('cucumber', 75.0, 'vegetables')
+weight = float(input('Вес товара: ')) # 2.75 = 2 kilograms and 750 grams
+print(cucumbers.product_price(weight))
